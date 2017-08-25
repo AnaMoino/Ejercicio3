58c4901a0bec697a45b38be6fe4596ce53183ebd
AnaMoino <anawin26@gmail.com>
2017-08-25 11:35:09 -0600

    Actualizando para ejercicio 3

---
+++ src/main/webapp/script1.js
@@ -0,0 +1,14 @@
+function readBrowserProperties(){
+    
+    var txt = "";
+txt += "Browser CodeName: " + navigator.appCodeName;
+txt += " Browser Name: " + navigator.appName;
+txt += " Browser Version: " + navigator.appVersion;
+txt += " Cookies Enabled: " + navigator.cookieEnabled;
+txt += " Browser Language: " + navigator.language;
+txt += " Browser Online: " + navigator.onLine;
+txt += " Platform: " + navigator.platform;
+    
+          $("#result").text(txt);   
+          readOS();
+}
---
+++ src/main/webapp/script2.js
@@ -0,0 +1,6 @@
+function readOS(){
+   var txt = "";
+   txt += "User-agent header: " + navigator.userAgent;
+   
+    $("#result2").text(txt);
+}
